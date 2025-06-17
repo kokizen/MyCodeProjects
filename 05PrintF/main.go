1a3ca2220b51aff00471686dcb247679e640b41d
kokizen <koki.alemino@gmail.com>
2025-06-17 15:19:10 +0100

    05Printf

---
+++ 05PrintF/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Joanne"
+	var age int = 30
+
+	fmt.Printf("age has value: %v and type: %T\n", age, age)
+	fmt.Printf("name has value: %v and type: %T\n", name, name)
+}
